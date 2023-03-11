703db1548149f642cb3022fe9e557f546b820d46
Ozan <ozancangu24@gmail.com>
2023-03-11 16:37:59 +0300

    -Quote model class created

---
+++ app/src/main/java/com/example/quotegenerator/model/data/Quote.kt
@@ -0,0 +1,3 @@
+package com.example.quotegenerator.model.data
+
+class Quote : ArrayList<QuoteItem>()
---
+++ app/src/main/java/com/example/quotegenerator/model/data/QuoteItem.kt
@@ -0,0 +1,6 @@
+package com.example.quotegenerator.model.data
+
+data class QuoteItem(
+    val author: String,
+    val text: String
+)
